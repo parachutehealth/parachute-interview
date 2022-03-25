db2ba8cd9e15b3c5d15b72bf835865bbae84eba2
JB2001216 <50648572+JB2001216@users.noreply.github.com>
2022-03-25 16:44:40 +0200

    [Ruby] Refactor auction and tests; Add additional test cases

--- ruby/lib/english_auction.rb
+++ ruby/lib/english_auction.rb
@@ -4,11 +4,20 @@ class EnglishAuction
     @io = io
     @users = []
     @bids = []
+    @current_high = 0
+    @current_winner = 'Nobody'
   end
 
   def start_auction
-    @io.out 'Enter the name of the first user, or blank to end'
+    prepare_users
+    do_bets
+    @io.out "Winner is #{@current_winner} with a bid of #{@current_high}"
+  end
 
+  private
+
+  def prepare_users
+    @io.out 'Enter the name of the first user, or blank to end'
     loop do
       user = @io.read.strip
       break if user.empty?
@@ -17,29 +26,7 @@ class EnglishAuction
       @io.out @users.last
       @io.out 'Enter the name of the next user, or blank to end'
     end
-
-    @io.out 'Current high bid is: 0 - Nobody'
-    current_high = 0
-    current_winner = 'Nobody'
-    loop do
-      current_user = @users.first
-      @io.out "#{current_user}, enter a bid (0 to pass):"
-      @users.push @users.shift
-
-      current_bid = @io.read.to_i
-      @bids.push(current_bid)
-      break if auction_over?
-      if current_bid > current_high
-        current_high = current_bid
-        current_winner = current_user
-      end
-      @io.out "Current high bid is: #{current_high} - #{current_winner}"
-    end
-
-    @io.out "Winner is #{current_winner} with a bid of #{current_high}"
   end
-
-  private
 
   def auction_over?
     return false unless @bids.length >= @users.length
@@ -47,4 +34,28 @@ class EnglishAuction
     recent_bids = @bids[start, @users.length]
     recent_bids.sum === 0
   end
+
+  def do_bets
+    print_current_high_bid
+    loop do
+      current_user = @users.shift
+      @io.out "#{current_user}, enter a bid (0 to pass):"
+      @users.push current_user
+
+      current_bid = @io.read.to_i
+      @bids.push(current_bid)
+      break if auction_over?
+      if current_bid > @current_high
+        @current_high = current_bid
+        @current_winner = current_user
+      end
+      print_current_high_bid
+    end
+  end
+
+  def print_current_high_bid
+    @io.out "Current high bid is: #{@current_high} - #{@current_winner}"
+  end
+
+
 end
--- ruby/spec/english_auction_spec.rb
+++ ruby/spec/english_auction_spec.rb
@@ -4,62 +4,90 @@ require_relative '../lib/english_auction'
 require_relative '../lib/input_output'
 
 RSpec.describe EnglishAuction do
+  def start(io)
+    main = EnglishAuction.new(io)
+    main.start_auction
+  end
+
+  def prepare_users(io, users)
+    prepare_inputs(io, *users, '')
+  end
+
+  def prepare_inputs(io, *inputs)
+    allow(io).to receive(:read).and_return(*inputs)
+    allow(io).to receive(:out)
+  end
+
+  def should_asks_for_user_and_show(io, users)
+    expect(io).to receive(:out).with('Enter the name of the first user, or blank to end').ordered
+    prepare_users(io, users)
+    should_show_users(io, users)
+  end
+
+  def should_show_users(io, users)
+    users.each do |user|
+      expect(io).to receive(:out).with(user).ordered
+      expect(io).to receive(:out).with('Enter the name of the next user, or blank to end').ordered
+    end
+  end
+
   describe 'an english auction' do
-    it 'prompts for the list of users and print them back' do
-      io = double(InputOutput)
-      expect(io).to receive(:out).with('Enter the name of the first user, or blank to end').ordered
-      allow(io).to receive(:read).and_return('Zell', 'Brian', '')
-      allow(io).to receive(:out)
-      expect(io).to receive(:out).with('Zell').ordered
-      expect(io).to receive(:out).with('Enter the name of the next user, or blank to end').ordered
-      expect(io).to receive(:out).with('Brian').ordered
-      expect(io).to receive(:out).with('Enter the name of the next user, or blank to end').ordered
-      main = EnglishAuction.new(io)
-      main.start_auction
+    context 'prompts for the list of users and print them back' do
+      it 'when 2 users' do
+        io = double(InputOutput)
+        should_asks_for_user_and_show(io, %w[Zell Brian])
+
+        start(io)
+      end
+      it 'when 3 users' do
+        io = double(InputOutput)
+        should_asks_for_user_and_show(io, %w[Zell Brian JB])
+
+        start(io)
+      end
+      it 'when 4 users' do
+        io = double(InputOutput)
+        should_asks_for_user_and_show(io, %w[Zell Brian JB NoJB])
+
+        start(io)
+      end
     end
 
     context 'after the contestants are entered' do
       it 'prints the current high bid' do
         io = double(InputOutput)
-        allow(io).to receive(:read).and_return('Zell', 'Brian', '')
-        allow(io).to receive(:out)
+        prepare_users(io, %w[Zell Brian])
         expect(io).to receive(:out).with('Current high bid is: 0 - Nobody')
-        main = EnglishAuction.new(io)
-        main.start_auction
+
+        start(io)
       end
 
       it 'prompts the first contestant to enter a bid' do
         io = double(InputOutput)
-        allow(io).to receive(:read).and_return('Zell', 'Brian', '')
-        allow(io).to receive(:out)
+        prepare_users(io, %w[Zell Brian])
         expect(io).to receive(:out).with('Zell, enter a bid (0 to pass):')
-        allow(io).to receive(:out)
-        main = EnglishAuction.new(io)
-        main.start_auction
+
+        start(io)
       end
     end
 
     context "asking for bids" do
       it "prompts the users for their bids" do
         io = double(InputOutput)
-        allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '0', '0')
-        allow(io).to receive(:out)
+        prepare_inputs(io, 'Zell', 'Brian', '', '3', '0', '0')
         expect(io).to receive(:out).with('Zell, enter a bid (0 to pass):').twice
         expect(io).to receive(:out).with('Brian, enter a bid (0 to pass):')
 
-        main = EnglishAuction.new(io)
-        main.start_auction
+        start(io)
       end
 
       context "when all contestants pass" do
         it "ends the auction and prints the winner" do
           io = double(InputOutput)
-          allow(io).to receive(:read).and_return('Zell', 'Brian', '','3', '4', '0', '0')
-          allow(io).to receive(:out)
+          prepare_inputs(io, 'Zell', 'Brian', '', '3', '4', '0', '0')
           expect(io).to receive(:out).with('Winner is Brian with a bid of 4')
 
-          main = EnglishAuction.new(io)
-          main.start_auction
+          start(io)
         end
       end
 
@@ -67,37 +95,30 @@ RSpec.describe EnglishAuction do
         context 'when it is a pass' do
           it 'does not record the bid' do
             io = double(InputOutput)
-            allow(io).to receive(:read).and_return('Zell', 'Brian', '',  '0', '0')
-            allow(io).to receive(:out)
+            prepare_inputs(io, 'Zell', 'Brian', '', '0', '0')
             expect(io).to receive(:out).with('Current high bid is: 0 - Nobody').twice
 
-            main = EnglishAuction.new(io)
-            main.start_auction
+            start(io)
           end
         end
 
         context 'when that bid is higher than the current high' do
             it 'displays that as the new high bid' do
               io = double(InputOutput)
-              allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '4', '0', '0')
-              allow(io).to receive(:out)
+              prepare_inputs(io, 'Zell', 'Brian', '', '3', '4', '0', '0')
               expect(io).to receive(:out).with('Current high bid is: 3 - Zell')
               expect(io).to receive(:out).with('Current high bid is: 4 - Brian')
-              main = EnglishAuction.new(io)
-              main.start_auction
+              start(io)
             end
           end
         context "when it is lower than the current high" do
           it "does not record the bid" do
             io = double(InputOutput)
-            allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '2', '0', '0')
-            allow(io).to receive(:out)
-            allow(io).to receive(:out)
+            prepare_inputs(io, 'Zell', 'Brian', '', '3', '2', '0', '0')
             expect(io).to receive(:out).with('Zell, enter a bid (0 to pass):')
             expect(io).to receive(:out).with('Current high bid is: 3 - Zell').at_least(2).times
 
-            main = EnglishAuction.new(io)
-            main.start_auction
+            start(io)
           end
         end
       end

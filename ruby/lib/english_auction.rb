3b7970ce6e874d2a3bc31398f83201297d235426
Hassan Ahmed <devhassan.ahmed@gmail.com>
2022-04-22 17:12:31 +0500

    Task 1 Code Refactoring

--- ruby/lib/english_auction.rb
+++ ruby/lib/english_auction.rb
@@ -1,50 +1,74 @@
 # frozen_string_literal: true
+
+# class to perform auction
 class EnglishAuction
   def initialize(io)
-    @io = io
-    @users = []
-    @bids = []
+    @io             = io
+    @users          = []
+    @bids           = []
+    @current_high   = 0
+    @current_winner = 'Nobody'
   end
 
   def start_auction
-    @io.out 'Enter the name of the first user, or blank to end'
+    # main method to initialize auction and decide the winner
+    set_auction_members
+    start_biding
+    display_winner
+  end
 
+  private
+
+  def set_auction_members
+    # method to set auction members
     loop do
+      @io.out 'Please Enter a User Name OR Enter Blank to End'
       user = @io.read.strip
       break if user.empty?
 
       @users.push user
       @io.out @users.last
-      @io.out 'Enter the name of the next user, or blank to end'
     end
-
-    @io.out 'Current high bid is: 0 - Nobody'
-    current_high = 0
-    current_winner = 'Nobody'
-    loop do
-      current_user = @users.first
-      @io.out "#{current_user}, enter a bid (0 to pass):"
-      @users.push @users.shift
-
-      current_bid = @io.read.to_i
-      @bids.push(current_bid)
-      break if auction_over?
-      if current_bid > current_high
-        current_high = current_bid
-        current_winner = current_user
-      end
-      @io.out "Current high bid is: #{current_high} - #{current_winner}"
-    end
-
-    @io.out "Winner is #{current_winner} with a bid of #{current_high}"
   end
 
-  private
+  def start_biding
+    # method to get bids from all users until they decide to pass
+    loop do
+      display_highest_bid
+      user = @users.first
+      @io.out "#{user}, enter a bid (0 to pass):"
+      @users.push @users.shift
+
+      user_bid = @io.read.to_i
+      @bids.push(user_bid)
+      break if auction_over?
+
+      set_highest_bid(user_bid, user) if user_bid > @current_high
+    end
+  end
 
   def auction_over?
+    # method to ensure all users have entered their bids and passed their turn
     return false unless @bids.length >= @users.length
+
     start = @users.length * -1
     recent_bids = @bids[start, @users.length]
-    recent_bids.sum === 0
+    recent_bids.sum.zero?
+  end
+
+  def set_highest_bid(user_bid, user)
+    # method to set winner of the auction
+    @current_high = user_bid
+    @current_winner = user
+  end
+
+  def display_winner
+    # method to display the winner
+    @io.out "Winner is #{@current_winner} with a bid of #{@current_high}"
+  end
+
+  def display_highest_bid
+    # method to display highest bid at any moment
+    @io.out "Current high bid is: #{@current_high} - #{@current_winner}"
   end
 end
--- ruby/spec/english_auction_spec.rb
+++ ruby/spec/english_auction_spec.rb
@@ -5,15 +5,19 @@ require_relative '../lib/input_output'
 
 RSpec.describe EnglishAuction do
   describe 'an english auction' do
+    let(:high_bid_prompt) { 'Current high bid is:' }
+    let(:bid_input_prompt) { ', enter a bid (0 to pass):' }
+    let(:input_string) { 'Please Enter a User Name OR Enter Blank to End' }
+
     it 'prompts for the list of users and print them back' do
       io = double(InputOutput)
-      expect(io).to receive(:out).with('Enter the name of the first user, or blank to end').ordered
+      expect(io).to receive(:out).with(input_string).ordered
       allow(io).to receive(:read).and_return('Zell', 'Brian', '')
       allow(io).to receive(:out)
       expect(io).to receive(:out).with('Zell').ordered
-      expect(io).to receive(:out).with('Enter the name of the next user, or blank to end').ordered
+      expect(io).to receive(:out).with(input_string).ordered
       expect(io).to receive(:out).with('Brian').ordered
-      expect(io).to receive(:out).with('Enter the name of the next user, or blank to end').ordered
+      expect(io).to receive(:out).with(input_string).ordered
       main = EnglishAuction.new(io)
       main.start_auction
     end
@@ -23,7 +27,7 @@ RSpec.describe EnglishAuction do
         io = double(InputOutput)
         allow(io).to receive(:read).and_return('Zell', 'Brian', '')
         allow(io).to receive(:out)
-        expect(io).to receive(:out).with('Current high bid is: 0 - Nobody')
+        expect(io).to receive(:out).with("#{high_bid_prompt} 0 - Nobody")
         main = EnglishAuction.new(io)
         main.start_auction
       end
@@ -32,29 +36,29 @@ RSpec.describe EnglishAuction do
         io = double(InputOutput)
         allow(io).to receive(:read).and_return('Zell', 'Brian', '')
         allow(io).to receive(:out)
-        expect(io).to receive(:out).with('Zell, enter a bid (0 to pass):')
+        expect(io).to receive(:out).with("Zell#{bid_input_prompt}")
         allow(io).to receive(:out)
         main = EnglishAuction.new(io)
         main.start_auction
       end
     end
 
-    context "asking for bids" do
-      it "prompts the users for their bids" do
+    context 'asking for bids' do
+      it 'prompts the users for their bids' do
         io = double(InputOutput)
         allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '0', '0')
         allow(io).to receive(:out)
-        expect(io).to receive(:out).with('Zell, enter a bid (0 to pass):').twice
-        expect(io).to receive(:out).with('Brian, enter a bid (0 to pass):')
+        expect(io).to receive(:out).with("Zell#{bid_input_prompt}").twice
+        expect(io).to receive(:out).with("Brian#{bid_input_prompt}")
 
         main = EnglishAuction.new(io)
         main.start_auction
       end
 
-      context "when all contestants pass" do
-        it "ends the auction and prints the winner" do
+      context 'when all contestants pass' do
+        it 'ends the auction and prints the winner' do
           io = double(InputOutput)
-          allow(io).to receive(:read).and_return('Zell', 'Brian', '','3', '4', '0', '0')
+          allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '4', '0', '0')
           allow(io).to receive(:out)
           expect(io).to receive(:out).with('Winner is Brian with a bid of 4')
 
@@ -67,9 +71,9 @@ RSpec.describe EnglishAuction do
         context 'when it is a pass' do
           it 'does not record the bid' do
             io = double(InputOutput)
-            allow(io).to receive(:read).and_return('Zell', 'Brian', '',  '0', '0')
+            allow(io).to receive(:read).and_return('Zell', 'Brian', '', '0', '0')
             allow(io).to receive(:out)
-            expect(io).to receive(:out).with('Current high bid is: 0 - Nobody').twice
+            expect(io).to receive(:out).with("#{high_bid_prompt} 0 - Nobody").twice
 
             main = EnglishAuction.new(io)
             main.start_auction
@@ -77,24 +81,61 @@ RSpec.describe EnglishAuction do
         end
 
         context 'when that bid is higher than the current high' do
-            it 'displays that as the new high bid' do
-              io = double(InputOutput)
-              allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '4', '0', '0')
-              allow(io).to receive(:out)
-              expect(io).to receive(:out).with('Current high bid is: 3 - Zell')
-              expect(io).to receive(:out).with('Current high bid is: 4 - Brian')
-              main = EnglishAuction.new(io)
-              main.start_auction
-            end
+          it 'displays that as the new high bid' do
+            io = double(InputOutput)
+            allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '4', '0', '0')
+            allow(io).to receive(:out)
+            expect(io).to receive(:out).with("#{high_bid_prompt} 3 - Zell")
+            expect(io).to receive(:out).with("#{high_bid_prompt} 4 - Brian")
+            main = EnglishAuction.new(io)
+            main.start_auction
           end
-        context "when it is lower than the current high" do
-          it "does not record the bid" do
+        end
+
+        context 'when it is lower than the current high' do
+          it 'does not record the bid' do
             io = double(InputOutput)
             allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '2', '0', '0')
             allow(io).to receive(:out)
             allow(io).to receive(:out)
-            expect(io).to receive(:out).with('Zell, enter a bid (0 to pass):')
-            expect(io).to receive(:out).with('Current high bid is: 3 - Zell').at_least(2).times
+            expect(io).to receive(:out).with("Zell#{bid_input_prompt}")
+            expect(io).to receive(:out).with("#{high_bid_prompt} 3 - Zell").at_least(2).times
+
+            main = EnglishAuction.new(io)
+            main.start_auction
+          end
+        end
+
+        context 'When both user enter same bid' do
+          it 'shows the first user as winner' do
+            io = double(InputOutput)
+            allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '3', '0', '0')
+            allow(io).to receive(:out)
+            expect(io).to receive(:out).with('Winner is Zell with a bid of 3')
+
+            main = EnglishAuction.new(io)
+            main.start_auction
+          end
+        end
+
+        context 'When a user enters a negative bid' do
+          it 'will discard that bid' do
+            io = double(InputOutput)
+            allow(io).to receive(:read).and_return('Zell', 'Brian', '', '3', '-3', '0', '0')
+            allow(io).to receive(:out)
+            allow(io).to receive(:out)
+            expect(io).to receive(:out).with("#{high_bid_prompt} 3 - Zell").at_least(1).times
+            main = EnglishAuction.new(io)
+            main.start_auction
+          end
+        end
+
+        context 'when both user passes invalid values' do
+          it 'does not record the bid' do
+            io = double(InputOutput)
+            allow(io).to receive(:read).and_return('Zell', 'Brian', '', 'Dummy', 'Dummy')
+            allow(io).to receive(:out)
+            expect(io).to receive(:out).with("#{high_bid_prompt} 0 - Nobody").twice
 
             main = EnglishAuction.new(io)
             main.start_auction
